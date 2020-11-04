038e5993bd9dfbcba650dd4b51eae4ceb298187d
Ray <rgarcia.7770@gmail.com>
2020-11-04 17:54:12 -0600

    Initial commit

---
+++ src/updates/views.py
@@ -0,0 +1,83 @@
+import json
+
+from django.core.serializers import serialize
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+from django.views.generic import View
+
+from superframes.mixins import JsonResponseMixin
+
+from .models import Update
+#def detial_view(request):
+    #return render(request, template, {}) # return JSON data  --> JS Object Notion
+    #return HttpResponse(get_template().render({}))
+
+# obj = Update.objects.get(id=1)
+
+def json_example_view(request):
+    '''
+    URI -- for a REST API
+    GET -- Retrieve
+    '''
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+    }
+    json_data = json.dumps(data)
+    #return JsonResponse(data)
+    return HttpResponse(json_data, content_type='application/json')
+
+
+
+class JsonCBV(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return JsonResponse(data)
+
+
+class JsonCBV2(JsonResponseMixin, View):
+     def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return self.render_to_json_response(data)
+
+
+
+class SerializedDetialView(View):
+     def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        data = serialize("json", [obj,], fields=('user', 'content'))
+        #print(data)
+        json_data = data
+        # data = {
+        #     "user": obj.user.username,
+        #     "content": obj.content
+        # }
+        # json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class SerializedListView(View):
+     def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        data = serialize("json", qs, fields=('user', 'content'))
+        print(data)
+        json_data = data
+        # data = {
+        #     "user": obj.user.username,
+        #     "content": obj.content
+        # }
+        #json_data = json.dumps(data)
+
+        return HttpResponse(json_data, content_type='application/json')
+
+
+
+
+
+
