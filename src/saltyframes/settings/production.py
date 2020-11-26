a9b037338a85f980801ce36adb707fedfde75f00
Ray <rgarcia.7770@gmail.com>
2020-11-26 01:03:08 -0600

    Fixed something

--- src/saltyframes/settings/production.py
+++ src/saltyframes/settings/production.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://salty-frames.herokuapp.com/']
+ALLOWED_HOSTS = ['salty-frames.herokuapp.com']
 
 APPEND_SLASH = True
 
