eb9c5bd71a65e1796af7f46be5e109be7da90986
Ray <rgarcia.7770@gmail.com>
2020-11-28 00:39:02 -0600

    Update database settings

--- saltyframes/settings/production.py
+++ saltyframes/settings/production.py
@@ -92,12 +92,8 @@ WSGI_APPLICATION = 'saltyframes.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
