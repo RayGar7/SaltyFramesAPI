cd447a6ba5363d214116690f25ec337e97278888
Ray <rgarcia.7770@gmail.com>
2020-12-23 11:49:41 -0600

    Started making the tables

--- soulcalibur_vi/urls.py
+++ soulcalibur_vi/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     #re_path(r'^moves/(?P<pk>\d+)/$', views.MoveView.as_view(), name='move_detail')
     #path('<slug:slug>/', api_views.CharacterView.as_view(), name='character_detail'),
     re_path(r'^characters-api/(?P<pk>\d+)/$', api_views.CharacterRetrieveView.as_view(), name='character_public_detail'),
+    #re_path(r'^character/(?P<pk>\d+)/$', views.character_view, name='character_detail'),
+    # ex: /polls/5/
+    #path('<int:character_id>/', views.detail, name='detail'),
+    path('<slug:slug>/', views.detail, name='detail'),
 ]
--- soulcalibur_vi/views.py
+++ soulcalibur_vi/views.py
@@ -7,3 +7,33 @@ def home(request):
 
     context = {'characters': characters, 'title': 'Soulcalibur VI API'}
     return render(request, 'soulcalibur_vi/home.html', context)
+
+
+# def character_view(request):
+#     print(request)
+#     return HttpResponse(r"hello")
+#     #character = Character.objects.get(source="")
+
+def detail(request, slug):
+    character = Character.objects.get(slug = slug)
+    sections = Section.objects.all().order_by('id')
+    moves = Move.objects.filter(character=character)
+    #print(len(moves))
+
+    # create a dictionary of arrays to create a frame data table, separated by the character's sections
+    moves_table = {}
+    for i in range(0, 9):
+        moves_table[sections[i].name] = []
+
+
+    for move in moves:
+        moves_table[move.section.name].append(move)
+    
+    # for debugging: if len(moves) matches this next number, you did it right
+    #print(len(moves_table[sections[0].name]) + len(moves_table[sections[1].name]) + len(moves_table[sections[2].name]) + len(moves_table[sections[3].name]) + len(moves_table[sections[4].name]) + len(moves_table[sections[5].name]) + len(moves_table[sections[6].name]) + len(moves_table[sections[7].name]) + len(moves_table[sections[8].name]))
+    
+    context = {
+        "moves_table": moves_table,
+        "name": character.name
+    }
+    return render(request, 'soulcalibur_vi/character.html', context)
