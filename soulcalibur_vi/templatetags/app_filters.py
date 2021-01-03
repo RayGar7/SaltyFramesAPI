41a908d80bba579351daf751c6a7e6802c82791b
Ray <rgarcia.7770@gmail.com>
2021-01-02 22:23:19 -0600

    Not done with the height_level_to_image filter but want to save the progress

--- soulcalibur_vi/templatetags/app_filters.py
+++ soulcalibur_vi/templatetags/app_filters.py
@@ -1,5 +1,6 @@
 from django import template
 from datetime import date, timedelta
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -17,3 +18,34 @@ def newline(value, arg):
             new_value += " "
             
     return new_value
+
+
+@register.filter(name='height_level_to_image', is_safe=True)
+def height_level_to_image(value):
+    """ Converts the string into an html formatted way of putting images inside the table cells """
+    
+    print(value)
+    #high = "img/sc-attributes/High.png"
+    base_dir = "img/sc-attributes/"
+
+    high = base_dir + "High.png"
+    mid = base_dir + "Mid.png"
+    low = base_dir + "Low.png"
+    spec_low = base_dir + "SpecLow.png"
+    spec_mid = base_dir + "SpecMid.png"
+    ss = base_dir + "SS.png"
+
+
+    # new_value = ""
+    # i = 0
+    # while (i < len(value)):        
+    #     if (value[i:i+3] == ":H:"):
+    #         new_value += high
+    #         i += 3
+    #     else:
+    #         pass
+
+    # print("new value: ")    
+    # print(new_value)        
+    # return mark_safe(new_value)
+    return mark_safe(high)
