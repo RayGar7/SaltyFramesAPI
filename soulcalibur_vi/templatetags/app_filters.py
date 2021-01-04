3b77bb8a9c4ef152ad19647b0367e4a0812fc130
Ray <rgarcia.7770@gmail.com>
2021-01-04 00:59:42 -0600

    Added the core functionality for the icon processing system

--- soulcalibur_vi/templatetags/app_filters.py
+++ soulcalibur_vi/templatetags/app_filters.py
@@ -20,12 +20,10 @@ def newline(value, arg):
     return new_value
 
 
-@register.filter(name='height_level_to_image', is_safe=True)
+@register.filter(name='height_level_to_image')
 def height_level_to_image(value):
-    """ Converts the string into an html formatted way of putting images inside the table cells """
+    """ Converts the height level string into an html formatted way of putting images inside the table cells """
     
-    print(value)
-    #high = "img/sc-attributes/High.png"
     base_dir = "img/sc-attributes/"
 
     high = base_dir + "High.png"
@@ -34,18 +32,161 @@ def height_level_to_image(value):
     spec_low = base_dir + "SpecLow.png"
     spec_mid = base_dir + "SpecMid.png"
     ss = base_dir + "SS.png"
+    gi = base_dir + "GI.png"
 
 
-    # new_value = ""
-    # i = 0
-    # while (i < len(value)):        
-    #     if (value[i:i+3] == ":H:"):
-    #         new_value += high
-    #         i += 3
-    #     else:
-    #         pass
+    new_value = ""
+    i = 0
+    length = len(value)
+    while (i < length): 
+        if (value[i:i+3] == ":H:"):
+            new_value += high
+            i += 3
+        elif (value[i:i+3] == ":M:"):
+            new_value += mid
+            i += 3
+        elif (value[i:i+3] == ":L:"):
+            new_value += low
+            i += 3
+        elif (value[i:i+4] == ":SL:"):
+            new_value += spec_low
+            i += 4
+        elif (value[i:i+4] == ":SM:"):
+            new_value += spec_mid
+            i += 4
+        elif (value[i:i+4] == ":GI:"):
+            new_value += gi
+            i += 4
+        elif (value[i:i+4] == ":SS:"):
+            new_value += ss
+            i += 4
+        else:
+            new_value += value[i]
+            i += 1
 
-    # print("new value: ")    
-    # print(new_value)        
-    # return mark_safe(new_value)
-    return mark_safe(high)
+    return new_value if len(new_value) else value
+
+@register.filter(name='command_to_image')
+def command_to_image(value):
+    """ Converts the command string into an html formatted way of putting images inside the table cells """
+    
+    base_dir = "img/sc-inputs/"
+
+    a = base_dir + "A.png"
+    b = base_dir + "B.png"
+    k = base_dir + "K.png"
+    g = base_dir + "G.png"
+
+    Ia = base_dir + "Ia.png"
+    Ib = base_dir + "Ib.png"
+    Ik = base_dir + "Ik.png"
+    Ig = base_dir + "Ig.png"
+
+    one = base_dir + "1.png"
+    two = base_dir + "2.png"
+    three = base_dir + "3.png"
+    four = base_dir + "4.png"
+    six = base_dir + "6.png"
+    seven = base_dir + "7.png"
+    eight = base_dir + "8.png"
+    nine = base_dir + "9.png"
+
+    Ione = base_dir + "I1.png"
+    Itwo = base_dir + "I2.png"
+    Ithree = base_dir + "I3.png"
+    Ifour = base_dir + "I4.png"
+    Isix = base_dir + "I6.png"
+    Iseven = base_dir + "I7.png"
+    Ieight = base_dir + "I8.png"
+    Inine = base_dir + "I9.png"
+
+    sc = base_dir + "SoulCharged.png"
+
+
+    new_value = ""
+    i = 0
+    length = len(value)
+    while (i < length): 
+        if (value[i:i+3] == ":1:"):
+            new_value += one
+            i += 3
+        elif (value[i:i+3] == ":2:"):
+            new_value += two
+            i += 3
+        elif (value[i:i+3] == ":3:"):
+            new_value += three
+            i += 3
+        elif (value[i:i+3] == ":4:"):
+            new_value += four
+            i += 3
+        elif (value[i:i+3] == ":6:"):
+            new_value += six
+            i += 3
+        elif (value[i:i+3] == ":7:"):
+            new_value += seven
+            i += 3
+        elif (value[i:i+3] == ":8:"):
+            new_value += eight
+            i += 3
+        elif (value[i:i+3] == ":9:"):
+            new_value += nine
+            i += 3
+
+        elif (value[i:i+3] == ":A:"):
+            new_value += a
+            i += 3
+        elif (value[i:i+3] == ":B:"):
+            new_value += b
+            i += 3
+        elif (value[i:i+3] == ":K:"):
+            new_value += k
+            i += 3
+        elif (value[i:i+3] == ":G:"):
+            new_value += g
+            i += 3
+
+        elif (value[i:i+5] == ":(A):"):
+            new_value += Ia
+            i += 5
+        elif (value[i:i+5] == ":(B):"):
+            new_value += Ib
+            i += 5
+        elif (value[i:i+5] == ":(K):"):
+            new_value += Ik
+            i += 5
+        elif (value[i:i+5] == ":(G):"):
+            new_value += Ig
+            i += 5
+
+        elif (value[i:i+5] == ":(1):"):
+            new_value += Ione
+            i += 5
+        elif (value[i:i+5] == ":(2):"):
+            new_value += Itwo
+            i += 5
+        elif (value[i:i+5] == ":(3):"):
+            new_value += Ithree
+            i += 5
+        elif (value[i:i+5] == ":(4):"):
+            new_value += Ifour
+            i += 5
+        elif (value[i:i+5] == ":(6):"):
+            new_value += Isix
+            i += 5
+        elif (value[i:i+5] == ":(7):"):
+            new_value += Iseven
+            i += 5
+        elif (value[i:i+5] == ":(8):"):
+            new_value += Ieight
+            i += 5
+        elif (value[i:i+5] == ":(9):"):
+            new_value += Inine
+            i += 5
+
+        else:
+            new_value += value[i]
+            i += 1
+
+    #print(new_value)
+
+    return new_value if len(new_value) else value
--- soulcalibur_vi/views.py
+++ soulcalibur_vi/views.py
@@ -5,6 +5,8 @@ from django import template
 
 register = template.Library()
 
+allowable_images = [":1:", ":2:", ":3:", ":4:", ":6:", ":7:", ":8:", ":9:", ":A:", ":B:", ":K:", ":G:", ":(A):", ":(B):", ":(K):", ":(G):", ":(1):", ":(2):", ":(3):", ":(4):", ":(6):", ":(7):", ":(8):", ":(9):"]
+
 def home(request):
     characters = Character.objects.all().order_by('name')
 
@@ -21,7 +23,8 @@ def detail(request, slug):
         "name": character.name,
         "image": character.image.url,
         "table_list": [],
-        "title": character.name
+        "title": character.name,
+        "allowable_images": allowable_images,
     }
 
     for i in range(0, 9):
@@ -29,6 +32,10 @@ def detail(request, slug):
     context['sectionless_table'] = []
 
     for move in moves:
+        #create a list of commands and height_levels from the respective strings so that I can use the for template tag on them
+        command_string_to_list(move)
+        height_level_string_to_list(move)
+
         if (move.section):
             if (move.section.name == "horizontal attack"):
                 context['table_list'][0]['moves_list'].append(move)
@@ -55,3 +62,44 @@ def detail(request, slug):
     #print(len(context['table_list'][0]['moves_list']) + len(context['table_list'][1]['moves_list']) + len(context['table_list'][2]['moves_list']) + len(context['table_list'][3]['moves_list']) + len(context['table_list'][4]['moves_list']) + len(context['table_list'][5]['moves_list']) + len(context['table_list'][6]['moves_list']) + len(context['table_list'][7]['moves_list']) + len(context['table_list'][8]['moves_list']) + len(context['sectionless_table']))
 
     return render(request, 'soulcalibur_vi/character-detail.html', context)
+
+
+def height_level_string_to_list(move):
+    value = move.height_level
+    height_level_list = []
+    i = 0
+    while (i < len(value)):
+        if (value[i] == ":" and value[i+2] == ":"):
+            height_level_list.append(value[i:i+3])
+            i += 3
+        elif (value[i] == ":" and value[i+3] == ":"):
+            height_level_list.append(value[i:i+4])
+            i += 4
+        else:
+            i += 1
+
+    move.height_level = height_level_list
+
+def command_string_to_list(move):
+    value = move.command
+    command_list = []
+    i = 0
+    while (i < len(value)):
+        if (i + 2 < len(value) and value[i:i+3] in allowable_images):
+            command_list.append(value[i:i+3])
+            i += 3
+        elif (i + 3 < len(value) and value[i:i+4] in allowable_images):
+            command_list.append(value[i:i+4])
+            i += 4
+        elif (i + 4 < len(value) and value[i:i+5] in allowable_images):
+            command_list.append(value[i:i+5])
+            i += 5
+        elif (i + 5 < len(value) and value[i:i+6] in allowable_images):
+            command_list.append(value[i:i+6])
+            i += 6
+        else:
+            command_list.append(value[i])
+            i += 1
+    #print(command_list)
+
+    move.command = command_list
