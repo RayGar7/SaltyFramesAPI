e4a0ded6b28d902643f5c4dfeb3633e75eec093b
Ray <rgarcia.7770@gmail.com>
2021-01-12 16:44:53 -0600

    Pretty much finished up the modal for the key

--- soulcalibur_vi/views.py
+++ soulcalibur_vi/views.py
@@ -31,10 +31,81 @@ allowable_images = [
 def home(request):
     characters = Character.objects.all().order_by('name')
 
-    context = {'characters': characters, 'title': 'Soulcalibur VI API'}
+    key = get_key()
+
+    context = {
+        'characters': characters, 
+        'inputs': key['inputs'],
+        'columns': key['columns'],
+        'title': 'Soulcalibur VI API'
+    }
+
     return render(request, 'soulcalibur_vi/home.html', context)
 
 def legend(request):
+    key = get_key()
+
+    context = {'inputs': key['inputs'], 'columns': key['columns'], 'title': "Legend"}
+
+    return render(request, 'soulcalibur_vi/legend.html', context)
+
+
+def detail(request, slug):
+    character = Character.objects.get(slug = slug)
+    # order_by('id') is a trick I use to order objects in the order they were saved
+    sections = Section.objects.all().order_by('id')     
+    moves = Move.objects.filter(character=character).order_by('id')
+    special_stances = SpecialStance.objects.filter(character=character)
+    special_states = SpecialState.objects.filter(character=character)
+    special_stance_abbreviations = [special_stance.abbreviation for special_stance in special_stances]
+    special_state_abbreviations = [special_state.abbreviation for special_state in special_states]
+    allowable_patterns = special_stance_abbreviations + special_state_abbreviations
+
+    context = {
+        "name": character.name,
+        "image": character.image.url,
+        "table_list": [],
+        "title": character.name,
+        "allowable_images": allowable_images,
+        "allowable_patterns": allowable_patterns,
+    }
+
+    for i in range(0, 9):
+        context['table_list'].append({'table_name': sections[i].name, 'moves_list': []})
+    context['sectionless_table'] = []
+
+    for move in moves:
+        #create a list of commands and height_levels from the respective strings so that I can use the for template tag on them
+        command_string_to_list(move=move, additional_patterns = allowable_patterns)
+        height_level_string_to_list(move=move)
+
+        if (move.section):
+            if (move.section.name == "horizontal attack"):
+                context['table_list'][0]['moves_list'].append(move)
+            elif (move.section.name == "vertical attack"):
+                context['table_list'][1]['moves_list'].append(move)
+            elif (move.section.name == "kick attack"):
+                context['table_list'][2]['moves_list'].append(move)
+            elif (move.section.name == "dual button attack"):
+                context['table_list'][3]['moves_list'].append(move)
+            elif (move.section.name == "8-way run"):
+                context['table_list'][4]['moves_list'].append(move)
+            elif (move.section.name == "special move"):
+                context['table_list'][5]['moves_list'].append(move)
+            elif (move.section.name == "throw"):
+                context['table_list'][6]['moves_list'].append(move)
+            elif (move.section.name == "reversal attack"):
+                context['table_list'][7]['moves_list'].append(move)
+            elif (move.section.name == "gauge attack"):
+                context['table_list'][8]['moves_list'].append(move)
+        else:
+            context['sectionless_table'].append(move)
+            
+    return render(request, 'soulcalibur_vi/character-detail.html', context)
+
+#helpers
+
+def get_key():
     base_dir = 'img/sc-inputs/'
 
     columns = [
@@ -210,63 +281,8 @@ def legend(request):
         },
     ]
 
-    context = {'inputs': inputs, 'columns': columns, 'title': "Legend"}
-    return render(request, 'soulcalibur_vi/legend.html', context)
+    return {'columns': columns, 'inputs_without_images': inputs_without_images, 'inputs': inputs}
 
-def detail(request, slug):
-    character = Character.objects.get(slug = slug)
-    # order_by('id') is a trick I use to order objects in the order they were saved
-    sections = Section.objects.all().order_by('id')     
-    moves = Move.objects.filter(character=character).order_by('id')
-    special_stances = SpecialStance.objects.filter(character=character)
-    special_states = SpecialState.objects.filter(character=character)
-    special_stance_abbreviations = [special_stance.abbreviation for special_stance in special_stances]
-    special_state_abbreviations = [special_state.abbreviation for special_state in special_states]
-    allowable_patterns = special_stance_abbreviations + special_state_abbreviations
-
-    context = {
-        "name": character.name,
-        "image": character.image.url,
-        "table_list": [],
-        "title": character.name,
-        "allowable_images": allowable_images,
-        "allowable_patterns": allowable_patterns,
-    }
-
-    for i in range(0, 9):
-        context['table_list'].append({'table_name': sections[i].name, 'moves_list': []})
-    context['sectionless_table'] = []
-
-    for move in moves:
-        #create a list of commands and height_levels from the respective strings so that I can use the for template tag on them
-        command_string_to_list(move=move, additional_patterns = allowable_patterns)
-        height_level_string_to_list(move=move)
-
-        if (move.section):
-            if (move.section.name == "horizontal attack"):
-                context['table_list'][0]['moves_list'].append(move)
-            elif (move.section.name == "vertical attack"):
-                context['table_list'][1]['moves_list'].append(move)
-            elif (move.section.name == "kick attack"):
-                context['table_list'][2]['moves_list'].append(move)
-            elif (move.section.name == "dual button attack"):
-                context['table_list'][3]['moves_list'].append(move)
-            elif (move.section.name == "8-way run"):
-                context['table_list'][4]['moves_list'].append(move)
-            elif (move.section.name == "special move"):
-                context['table_list'][5]['moves_list'].append(move)
-            elif (move.section.name == "throw"):
-                context['table_list'][6]['moves_list'].append(move)
-            elif (move.section.name == "reversal attack"):
-                context['table_list'][7]['moves_list'].append(move)
-            elif (move.section.name == "gauge attack"):
-                context['table_list'][8]['moves_list'].append(move)
-        else:
-            context['sectionless_table'].append(move)
-            
-    return render(request, 'soulcalibur_vi/character-detail.html', context)
-
-#helpers
 def height_level_string_to_list(move):
     if (move.height_level):
         value = move.height_level
